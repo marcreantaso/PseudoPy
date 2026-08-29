895fe5aff0e00d9051c3afcdae407678042b59e0
Mark Bautista <mark.bautista@university.edu.ph>
2026-08-29 22:46:25 +0800

    feat: default to automatic darkmode and align learning analytics visuals

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Offline-first caching strategy
    ============================================================ */
 
-const CACHE_NAME = 'pseudopy-v33';
+const CACHE_NAME = 'pseudopy-v34';
 const LOCAL_ASSETS = [
     './',
     './index.html',
